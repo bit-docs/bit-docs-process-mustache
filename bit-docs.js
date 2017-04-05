1c944e6404061d752d883f8ecf12cb9c70dd4a1d
Joel Kuzmarski <leoj3n@gmail.com>
2017-04-05 10:50:41 -0500

    prelim docs

--- bit-docs.js
+++ bit-docs.js
@@ -1,5 +1,15 @@
 var processMustache = require("./process-mustache");
 
+/**
+ * @module {function} bit-docs-process-mustache
+ * @parent plugins
+ *
+ * @description Process content in mustache files.
+ *
+ * @body
+ *
+ * TBD
+ */
 module.exports = function(bitDocs) {
   bitDocs.register("processor", processMustache);
 };
